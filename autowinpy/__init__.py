567eb4cc18d28de3753a2fd6745e2ccfcbdc43b9
soma0sd <soma0sd@gmail.com>
2019-12-17 19:46:49 +0900

    기본 함수 추가

--- autowinpy/__init__.py
+++ autowinpy/__init__.py
@@ -7,10 +7,14 @@ import sys as _sys
 from . import _win32 as win32
 from . import _tk as tk
 from ._classes import Gui, Template
+from ._core import (
+    _set_windows_dpi,
+    window_list,
+    find_window
+)
 
 __version__ = "0.3.0"
 
-from ._core import (_set_windows_dpi)
 if _sys.getwindowsversion().major > 8:
     __dpi_scale_factor__ = _set_windows_dpi()
 else:
--- autowinpy/_core.py
+++ autowinpy/_core.py
@@ -3,8 +3,14 @@ AutoWinPy::CORE
 ===============
 
 * 패키지 초기화
+* 기본 함수
 """
+from . import _win32
+from ._classes import Gui
 from ctypes import windll
+from typing import List
+import re
+
 
 def _set_windows_dpi() -> float:
   """[private] windows 8.1 이상을 위한 DPI 조정.
@@ -22,3 +28,22 @@ def _set_windows_dpi() -> float:
   user32.SetProcessDPIAware()
   scaled = user32.GetDpiForSystem()
   return scaled / native
+
+def window_list() -> List["autowinpy.Gui"]:
+  """윈도우 목록 출력
+
+  Returns:
+    ([:class:`autowinpy.Gui`]) 현재 제어가능한 창 목록
+  """
+  guis: List[Gui] = [Gui(h) for h in _win32.get_windows()]
+  return [gui for gui in guis if gui.is_active]
+
+def find_window(name_re:str) -> List["autowinpy.Gui"]:
+  """정규표현식으로 윈도우를 찾습니다.
+
+  Returns:
+    ([:class:`autowinpy.Gui`]) 일치하는 이름을 가진
+    윈도우 목록
+  """
+  _rex = re.compile(name_re)
+  return [win for win in window_list() if _rex.match(win.name)]
